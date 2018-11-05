21a79e95b7880374a275802846c6a4b454b717a4
Jack <jackmeyer@live.com.au>
2018-11-05 14:57:05 +1100

    welp

---
+++ BankMateTests/BankMateTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BankMateTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankMateTests
+{
+    [TestClass]
+    public class BankMateTests
+    {
+        [TestMethod]
+        public void TestAddUser()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
